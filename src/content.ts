a5b6c72ff2a1e3b036bdb93122654a7ba750b729
Valentin Degenne <vdegenne@gmail.com>
2025-04-20 11:34:52 +0200

    mini gamepad

--- src/content.ts
+++ src/content.ts
@@ -1,1 +1,13 @@
-console.log('to implement');
+import {MiniGamepad, Mode} from '@vdegenne/mini-gamepad';
+
+const minigp = new MiniGamepad();
+
+minigp.onConnect((gamepad) => {
+	const {LEFT_BUTTONS_TOP} = gamepad.mapping;
+
+	gamepad.before(LEFT_BUTTONS_TOP, ({mode}) => {
+		if (mode === Mode.NORMAL) {
+			window.close();
+		}
+	});
+});
